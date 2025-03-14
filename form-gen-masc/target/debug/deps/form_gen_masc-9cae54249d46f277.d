2d039d97bff3162295455995705f973feaccd235
Vyom <150064126+svyoma@users.noreply.github.com>
2025-03-14 20:23:11 +0530

    Added masculine form generator
    Added masculine form generator for a-ending, i-ending and u-ending words.

---
+++ form-gen-masc/target/debug/deps/form_gen_masc-9cae54249d46f277.d
@@ -0,0 +1,5 @@
+D:\prakrit-noun\form-gen-masc\target\debug\deps\libform_gen_masc-9cae54249d46f277.rmeta: src/main.rs
+
+D:\prakrit-noun\form-gen-masc\target\debug\deps\form_gen_masc-9cae54249d46f277.d: src/main.rs
+
+src/main.rs:
---
+++ form-gen-masc/target/debug/deps/form_gen_masc-bb121495e19aaeca.d
@@ -0,0 +1,5 @@
+D:\prakrit-noun\form-gen-masc\target\debug\deps\libform_gen_masc-bb121495e19aaeca.rmeta: src/main.rs
+
+D:\prakrit-noun\form-gen-masc\target\debug\deps\form_gen_masc-bb121495e19aaeca.d: src/main.rs
+
+src/main.rs:
